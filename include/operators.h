0562d20aaa1da5aadf830fb78d5e9ff50c47e59b
Kaiwen Wang <kaiwen3451@gmail.com>
2025-07-21 12:14:26 -0400

    a

--- include/operators.h
+++ include/operators.h
@@ -6,7 +6,6 @@
 
 typedef float sample_t;
 
-
 void lin(const sample_t *in,
          const sample_t *W,
          const sample_t *b,
@@ -14,12 +13,4 @@ void lin(const sample_t *in,
          size_t          in_dim,
          size_t          out_dim);
 
-void conv(const sample_t *in,
-          const sample_t *ker,
-          sample_t       *out,
-          size_t          i_rows,
-          size_t          i_cols,
-          size_t          k_rows,
-          size_t          k_cols);
-
-#endif // OPERATORS_H
+#endif 
--- src/operators.c
+++ src/operators.c
@@ -19,33 +19,3 @@ void lin(const sample_t *in,
     }
 }
 
-void conv(const sample_t *in,
-          const sample_t *ker,
-          sample_t       *out,
-          size_t          i_rows,
-          size_t          i_cols,
-          size_t          k_rows,
-          size_t          k_cols) {
-
-    ptrdiff_t p_row = (ptrdiff_t)(k_rows - 1) / 2;
-    ptrdiff_t p_col = (ptrdiff_t)(k_cols - 1) / 2;
-
-    for (size_t i = 0; i < i_rows; i++) {
-        for (size_t j = 0; j < i_cols; j++) {
-            sample_t acc = 0;
-            for (size_t ki = 0; ki < k_rows; ki++) {
-                for (size_t kj = 0; kj < k_cols; kj++) {
-                    ptrdiff_t in_r = (ptrdiff_t)i + (ptrdiff_t)ki - p_row;
-                    ptrdiff_t in_c = (ptrdiff_t)j + (ptrdiff_t)kj - p_col;
-                    if (in_r >= 0 && in_r < (ptrdiff_t)i_rows &&
-                        in_c >= 0 && in_c < (ptrdiff_t)i_cols) {
-                        acc += in[(size_t)in_r * i_cols + (size_t)in_c]
-                             * ker[ki * k_cols + kj];
-                    }
-                }
-            }
-            out[i * i_cols + j] = acc;
-        }
-    }
-}
-
